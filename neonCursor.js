daa70bb656c9d12a2aac661606ed9ba923c2cd5a
Teerth <157950924+teerthjj912@users.noreply.github.com>
2024-08-31 19:46:39 +0530

    Add files via upload

---
+++ neonCursor.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  neonCursor({
+    el: document.getElementById("app"),
+    shaderPoints: 16,
+    curvePoints: 80,
+    curveLerp: 0.5,
+    radius1: 5,
+    radius2: 30,
+    velocityTreshold: 10,
+    sleepRadiusX: 100,
+    sleepRadiusY: 100,
+    sleepTimeCoefX: 0.0025,
+    sleepTimeCoefY: 0.0025,
+  });
+});
